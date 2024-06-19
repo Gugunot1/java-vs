1fff8288cffa33be6bf709342a98ca908c21010e
Gugunot1 <valerasevkan08@gmail.com>
2024-06-19 20:12:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ filename = "1622199289.jpg"
 with open(filename, "rb") as palm_1_file:
     palm_1_image = Image(palm_1_file)
 
+mtime = os.path.getmtime(filename)
 if palm_1_image.has_exif:
     status = f"contains EXIF (version {palm_1_image.exif_version}) information."
+    mtime = os.path.getmtime(filename)
+    print('date_from_exif:', mtime)
 else:
     status = "does not contain any EXIF information."
 print(f"Image {filename} {status}")
-mtime = os.path.getmtime(filename)
-print('Exif data:',mtime)
 mtime_readable = datetime.fromtimestamp(mtime)
-print('Modification:',mtime_readable)
+print('Modification_date:',mtime_readable)
 c_timestamp = os.path.getctime(filename)
-print('Creation:',c_timestamp)
+c_time = datetime.fromtimestamp(c_timestamp)
+print('Creation_date:',c_time)
+
 
 
