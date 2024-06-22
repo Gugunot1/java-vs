9f00c0c8ed05be8e895915eb0fede99646d3f705
Gugunot1 <valerasevkan08@gmail.com>
2024-06-22 21:11:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import os
 from datetime import datetime
 from exif import Image
 
-filename = "1622199289.jpg"
-with open(filename, "rb") as palm_1_file:
+with open("DJI_0206.JPG", "rb") as palm_1_file:
     palm_1_image = Image(palm_1_file)
 
-palm_1_image.has_exif
-dtime = os.path.getmtime(filename)
-dtime_readable = datetime.fromtimestamp(dtime)
-print('date_from_exif:', dtime_readable)
+images = [palm_1_image]
 
-mtime = os.path.getmtime(filename)
+image = "DJI_0206.JPG"
+for index, image in enumerate(images):
+    if image.has_exif:
+        status = f"contains EXIF (version {image.exif_version}) information."
+    else:
+        status = "does not contain any EXIF information."
+    print(f"Image  {status}")
+    print(f"{image.datetime_original}.")
+image = "DJI_0206.JPG"
+mtime = os.path.getmtime(image)
 mtime_readable = datetime.fromtimestamp(mtime)
 print('Modification_date:',mtime_readable)
-c_timestamp = os.path.getctime(filename)
+c_timestamp = os.path.getctime(image)
 c_time = datetime.fromtimestamp(c_timestamp)
 print('Creation_date:',c_time)
 
