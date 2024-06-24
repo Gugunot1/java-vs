791bb7d1b14fcf585d8eb8dfcb110e07edba65b9
Gugunot1 <valerasevkan08@gmail.com>
2024-06-24 21:41:54 +0300

    Add files via upload

---
+++ date_from_exif.py
@@ -0,0 +1,23 @@
+import exif
+
+def date_from_exif(file_name):
+    try:
+        with open(file_name, 'rb') as f:
+            image = exif.Image(f)
+            if 'EXIF' in image.tags:
+                exif_data = image.tags['EXIF']
+                if 'DateTime' in exif_data:
+                    date_time = exif_data['DateTime']
+                    if date_time is not None:
+                        print(date_time)
+                    else:
+                        print('null')
+                else:
+                    print('null')
+            else:
+                print('null')
+    except Exception as e:
+     print(e)
+if __name__ == '__main__':
+    file_name = 'DJI_0160.JPG'
+    date_from_exif(file_name)
