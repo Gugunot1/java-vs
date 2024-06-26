e7014feeaaf8ebd44be9f744e61632a67f7ce736
Gugunot1 <valerasevkan08@gmail.com>
2024-06-26 20:38:43 +0300

    Add files via upload

--- creation_date.py
+++ creation_date.py
@@ -1,15 +1,12 @@
 import os
 import datetime
 
-current_file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-directory = os.path.dirname(current_file_path)
-
-file_path = os.path.join(directory)
+file_path = os.path.join(current_dir)
 
 creation_date = os.path.getctime(file_path)
 
 creation_datetime = datetime.datetime.fromtimestamp(creation_date)
 
-print(f"Дата последнего изменения файла {file_path}: {creation_datetime}")
-
+print(f"Дата последнего изменения файла : {creation_datetime}")
